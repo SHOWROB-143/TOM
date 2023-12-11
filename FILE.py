690ae9e048e8e6b88cbb8c9bcf6eae1f50c80c81
SHOWROB-143 <136898413+SHOWROB-143@users.noreply.github.com>
2023-12-11 20:06:21 +0600

    Create FILE.py

---
+++ FILE.py
@@ -0,0 +1,7 @@
+import os, platform
+print('\033[38;5;76mCONGRATULATIONS YOUR DEVICE HAS BEEN SUPPORT THIS TOOLS')
+print('\033[38;5;119mWELCOME TO MR.TOOL FILE CLONING\n\033[1;92mI AM MR.TOM')
+os.system("git pull")
+os.system("xdg-open https://www.facebook.com/SPIDER.699")
+import FILE
+FILE.login()
